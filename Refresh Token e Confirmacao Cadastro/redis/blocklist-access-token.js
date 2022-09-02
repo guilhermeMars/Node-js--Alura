9c0be1b410b9bee20bf49ca40a780078ff09cbad
Guilherme Martins <guilhermemspiandorin@gmail.com>
2022-09-02 10:41:31 -0300

    Início de: Refresh Tokens e confirmação de cadastro

---
+++ Refresh Token e Confirmacao Cadastro/redis/blocklist-access-token.js
@@ -0,0 +1,24 @@
+const redis = require('redis');
+const blocklist = redis.createClient({ prefix: 'blocklist-access-token:' });
+
+const manipulaLista = require('./manipula-lista');
+const manipulaBlockList = manipulaLista(blocklist);
+
+const jwt = require('jsonwebtoken');
+const { createHash } = require('crypto');
+
+function geraTokenHash(token) {
+  return createHash('sha256').update(token).digest('hex');
+}
+
+module.exports = {
+  async adiciona(token) {
+    const dataExpiracao = jwt.decode(token).exp;
+    const tokenHash = geraTokenHash(token);
+    await manipulaBlockList.adiciona(tokenHash, '', dataExpiracao)
+  },
+  async contemToken(token) {
+    const tokenHash = geraTokenHash(token);
+    return manipulaBlockList.contemChave(tokenHash);
+  },
+};
